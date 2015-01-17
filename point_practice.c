3ffd2f7c6465a06a4465e34a201be9c7eeca878d
victor <lncwwn@gmail.com>
2015-01-17 14:41:29 +0800

    refined point practice and string array

--- point_practice.c
+++ point_practice.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int *p, i = 3;
+    int* p, i = 3;
+    char string[] = "abcdefg"; // {"abcdefg"}
+    //char *string;
+    //gets(string);
     p = &i;
     printf("%p\n", p);
+    printf("%p\n", *&p);
+    printf("%d\n", *p);
+    printf("%lu\n", sizeof string);
+    puts(string);
     return 0;
 }

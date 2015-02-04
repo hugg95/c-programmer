9daea071e4ce663c1e8b9714d6c953f2d3e34ff5
victor.li <lncwwn@gmail.com>
2015-02-04 10:48:13 +0800

    added static practice

---
+++ static_practice1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "static_practice1.h"
+
+static int age = 100;
+
+void printAge() {
+    printf("age is %d\n", age);
+}
---
+++ static_practice2.c
@@ -0,0 +1,7 @@
+#include "static_practice1.h"
+
+int main() {
+    printAge();
+    //printf("%d", age);
+    return 0;
+}
